43e90887ea8663aa11c13d4d0f2d64f7c4b6f1d8
nikhitanelavalli575 <nikhitanelavalli1997@gmail.com>
2018-06-25 14:17:00 -0700

    third commit

--- main.js
+++ main.js
@@ -13,10 +13,31 @@ function loadJson(file,callback){
 loadJson("data.json",function(text){
   let data= JSON.parse(text);
   console.log(data);
+  fun_career(data.CareerObjective);
   fun_education(data.education);
+  fun_achieve(data.Achievments);
+  fun_tech(data.TechnicalSkills);
+
 });
 var div2=document.getElementById("child2");
+function fun_career(career){
+  var list3=document.createElement("h2");
+  list3.textContent="CareerObjective";
+  div2.appendChild(list3);
+  var listt=document.createElement("hr");
+  div2.appendChild(listt);
+
+  var list=document.createElement("ul");
+  div2.appendChild(list);
+  var list6=document.createElement("p");
+  list6.textContent=career.info;
+  list.appendChild(list6);
+  var listts=document.createElement("hr");
+  div2.appendChild(listts);
+
+}
 function fun_education(edu){
+
 
   var lis=document.createElement("h2");
   lis.textContent="Educational Qualifications";
@@ -43,7 +64,39 @@ for(var i=0;i<edu.length;i++){
   list.appendChild(listItem3);
 
 
+}
+}
+function fun_achieve(ach)
+{
+  var hori=document.createElement("h2");
+  hori.textContent="Achievments";
+  div2.appendChild(hori);
+  var head=document.createElement("hr");
+  div2.appendChild(head);
 
+  var list=document.createElement("ul");
+  div2.appendChild(list);
+
+  for(var i=0;i<ach.length;i++){
+    var listite=document.createElement("li");
+    listite.innerHTML=ach[i].name;
+    list.appendChild(listite);
 
 }
+}
+function fun_tech(skills)
+{
+  var horiz=document.createElement("h2");
+  horiz.textContent="Technical Skills";
+  div2.appendChild(horiz);
+  var heads=document.createElement("hr");
+  div2.appendChild(heads);
+  var tr="";
+  var table=document.createElement("table");
+  div2.appendChild(table);
+  for(var i=0;i<skills.length;i++)
+  {
+    tr+="<tr><td>"+skills[i].name+"</td><td>"+skills[i].Data+"</td></tr>"
+  }
+  table.innerHTML=tr;
 }
